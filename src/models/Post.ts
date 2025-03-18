15368d3bb4865781bfb1c82972e022eb3d00472c
Igor Chakrygin <igor@chakrygin.ru>
2025-03-18 22:55:40 +0300

    Fix Post

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -6,10 +6,10 @@ export interface Post {
   readonly image?: string;
   readonly title: string;
   readonly href: string;
-  readonly categories: readonly Link[];
+  readonly categories: Link[];
   readonly author?: string;
   readonly date?: moment.Moment;
-  readonly description?: readonly string[];
-  readonly links?: readonly Link[];
-  readonly tags?: readonly string[];
+  readonly description?: string[];
+  readonly links?: Link[];
+  readonly tags?: string[];
 }
